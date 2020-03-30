ff5c4448834e7c62764b03814879fca910cf698a
R D <rajan1106@yahoo.com>
2020-03-30 12:57:22 -0400

    Root REST call returns the name of the microservice

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Test Microservice"', () => {
+      expect(appController.getHello()).toBe('Test Microservice');
     });
   });
 });
