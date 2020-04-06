a59fa7e3d0a0df615888ca2bcf052f4eeba9f4ae
R D <rajan1106@yahoo.com>
2020-04-06 09:48:07 -0400

    Fixed APIs, path changed to /apis/test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('apis/test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Headers() headers): string {
+    console.log(headers)
     return this.appService.getHello();
   }
 
